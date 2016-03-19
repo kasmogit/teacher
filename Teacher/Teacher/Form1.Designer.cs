dffcfdff466be0b5fe21c236cb0fa404a89f13a8
Daisuke Kashima <Daisuke Kashima>
2016-03-19 20:57:28 +0900

    【teacher】かすもコミット 新規

---
+++ Teacher/Teacher/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Teacher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.menuButton1 = new Teacher.MenuButton();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Font = new System.Drawing.Font("MS UI Gothic", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(495, 331);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(238)))), ((int)(((byte)(170)))));
+            this.panel2.Controls.Add(this.menuButton1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(213, 331);
+            this.panel2.TabIndex = 0;
+            // 
+            // menuButton1
+            // 
+            this.menuButton1.ButtonText = "生徒名簿";
+            this.menuButton1.Font = new System.Drawing.Font("MS UI Gothic", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.menuButton1.HoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(68)))), ((int)(((byte)(68)))));
+            this.menuButton1.Location = new System.Drawing.Point(8, 8);
+            this.menuButton1.MainColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(204)))), ((int)(((byte)(204)))));
+            this.menuButton1.Name = "menuButton1";
+            this.menuButton1.Size = new System.Drawing.Size(183, 58);
+            this.menuButton1.TabIndex = 0;
+            this.menuButton1.TextLocation = new System.Drawing.Point(10, 10);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.BlanchedAlmond;
+            this.ClientSize = global::Teacher.Properties.Settings.Default.MySize;
+            this.Controls.Add(this.panel1);
+            this.DataBindings.Add(new System.Windows.Forms.Binding("Location", global::Teacher.Properties.Settings.Default, "MyLocatiton", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(248)))), ((int)(((byte)(238)))));
+            this.Location = global::Teacher.Properties.Settings.Default.MyLocatiton;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "先生支援ツール";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private MenuButton menuButton1;
+    }
+}
+
---
+++ Teacher/Teacher/MenuButton.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Teacher
+{
+    public partial class MenuButton : UserControl
+    {
+        // 定数
+        public const int Radius = 24;
+        public const int ColorDivCnt = 48;
+
+        // フィールド
+        public Color Color;
+        public int ColorRatio;
+
+        public Thread ThreadAnimeColorUp;
+        public Thread ThreadAnimeColorDown;
+
+        // プロパティ
+        public string ButtonText { get; set; }
+        public Point TextLocation { get; set; }
+        public Color MainColor { get; set; }
+        public Color HoverColor { get; set; }
+
+        // コンストラクタ
+        public MenuButton()
+        {
+            // プロパティデフォルト
+            TextLocation = new Point(10, 10);
+            Text = "";
+            MainColor = Color.FromArgb(255, 192, 192);
+            HoverColor = Color.FromArgb(255, 64, 64);
+
+            // 初期化
+            Color = MainColor;
+            ColorRatio = 0;
+
+            // ダブルバッファリング有効
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+            SetStyle(ControlStyles.UserPaint, true);
+            SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+
+            InitializeComponent();
+        }
+
+        // 描画
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            // Console.WriteLine(TextLocation);
+
+            Graphics g = e.Graphics;
+            SolidBrush b = new SolidBrush(Color);
+
+            g.FillRectangle(b, Radius / 2, 0, this.Width - Radius, this.Height);
+            g.FillRectangle(b, 0, Radius / 2, this.Width, this.Height - Radius);
+
+            g.FillPie(b, 0, 0, Radius, Radius, 0, -180);
+            g.FillPie(b, this.Width - Radius, 0, Radius, Radius, 0, -180);
+
+            g.FillPie(b, 0, this.Height - Radius, Radius, Radius, 0, 180);
+            g.FillPie(b, this.Width - Radius, this.Height - Radius, Radius, Radius, 0, 180);
+
+            g.DrawString(ButtonText, this.Font, Brushes.Black, TextLocation);
+        }
+
+        // マウスオーバー
+        protected override void OnMouseEnter(EventArgs e)
+        {
+            if (ThreadAnimeColorDown != null)
+            {
+                ThreadAnimeColorDown.Abort();
+                ThreadAnimeColorDown.Join();
+                ThreadAnimeColorDown = null;
+            }
+            if (ThreadAnimeColorUp != null)
+            {
+                return;
+            }
+
+            ThreadAnimeColorUp = new Thread(new ThreadStart(AnimeColorUp));
+            ThreadAnimeColorUp.Start();
+        }
+
+        // マウスアウト
+        protected override void OnMouseLeave(EventArgs e)
+        {
+            if (ThreadAnimeColorUp != null)
+            {
+                ThreadAnimeColorUp.Abort();
+                ThreadAnimeColorUp.Join();
+                ThreadAnimeColorUp = null;
+            }
+            if (ThreadAnimeColorDown != null)
+            {
+                return;
+            }
+
+            ThreadAnimeColorDown = new Thread(new ThreadStart(AnimeColorDown));
+            ThreadAnimeColorDown.Start();
+        }
+
+        // 色アニメーションUp
+        private void AnimeColorUp()
+        {
+            while(ColorRatio <= ColorDivCnt)
+            {
+                Color = GetRatioAnimeColor();
+                Invalidate(Region);
+                ColorRatio++;
+                Thread.Sleep(10);
+            }
+            ColorRatio = ColorDivCnt;
+        }
+
+        // 色アニメーションDown
+        private void AnimeColorDown()
+        {
+            while (ColorRatio >= 0)
+            {
+                Color = GetRatioAnimeColor();
+                Invalidate(Region);
+                ColorRatio--;
+                Thread.Sleep(10);
+            }
+            ColorRatio = 0;
+        }
+
+        // アニメーション色取得
+        private Color GetRatioAnimeColor()
+        {
+            double mr = (double)(ColorDivCnt - ColorRatio) / ColorDivCnt;
+            double hr = (double)ColorRatio / ColorDivCnt;
+
+            double r = MainColor.R * mr + HoverColor.R * hr;
+            double g = MainColor.G * mr + HoverColor.G * hr;
+            double b = MainColor.B * mr + HoverColor.B * hr;
+
+            return Color.FromArgb((int)r, (int)g, (int)b);
+        }
+    }
+}
